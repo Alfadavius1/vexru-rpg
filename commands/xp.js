31aef01b0010833c88023c4da95cad5457f0ae77
Alfadavius1 <martincorej@seznam.cz>
2026-05-25 11:41:48 +0200

    Create xp.js

---
+++ commands/xp.js
@@ -0,0 +1,26 @@
+const { getUser, getNeededXP } = require("../core/xp");
+
+module.exports = {
+    name: "xp",
+
+    async execute(client, channel, user) {
+        const username = user.username.toLowerCase();
+
+        // Získání dat uživatele
+        const data = await getUser(username);
+
+        if (!data) {
+            return client.say(
+                channel,
+                `@${username} zatím nemáš žádné XP. Napiš něco do chatu a začneš je získávat!`
+            );
+        }
+
+        const needed = getNeededXP(data.level);
+
+        client.say(
+            channel,
+            `@${username} • Level: ${data.level} • XP: ${data.xp}/${needed}`
+        );
+    }
+};
