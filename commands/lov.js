8f6d49c219448249ef83f893a833500202e777e1
Alfadavius1 <martincorej@seznam.cz>
2026-05-25 09:30:23 +0200

    Update lov.js

--- commands/lov.js
+++ commands/lov.js
@@ -15,6 +15,21 @@ module.exports = {
             return client.say(channel, `@${user.username} počkej ještě ${cd.toFixed(1)}s.`);
         }
 
+        // Načtení DB
+        const db = JSON.parse(fs.readFileSync("./data/users.json"));
+        const key = user.username.toLowerCase();
+
+        // ⭐ Pokud hráč neexistuje → vytvoříme celý profil
+        if (!db[key]) {
+            db[key] = {
+                level: 1,
+                xp: 0,
+                gold: 0,
+                gear: {},
+                inventory: []
+            };
+        }
+
         // Náhodný základní item
         const baseItems = JSON.parse(fs.readFileSync("./data/baseItems.json"));
         const baseItem = baseItems[Math.floor(Math.random() * baseItems.length)];
@@ -32,18 +47,14 @@ module.exports = {
         };
 
         // Přidání itemu do inventáře
-        addItem(user.username.toLowerCase(), item);
+        addItem(key, item);
 
         // Výpočet odměn
         const xpGain = 5 + (info.buffs.xp || 0);
         const goldGain = 3 + (info.buffs.gold || 0);
 
         // Uložení goldů
-        const db = JSON.parse(fs.readFileSync("./data/users.json"));
-        const key = user.username.toLowerCase();
-
-        if (!db[key]) db[key] = {};
-        db[key].gold = (db[key].gold || 0) + goldGain;
+        db[key].gold += goldGain;
 
         fs.writeFileSync("./data/users.json", JSON.stringify(db, null, 2));
 
