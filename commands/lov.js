924b2dc837593b8ff93afaaba902dd003f2b2aed
Alfadavius1 <martincorej@seznam.cz>
2026-05-25 17:32:12 +0200

    Update lov.js

--- commands/lov.js
+++ commands/lov.js
@@ -1,31 +1,66 @@
-const { getRandomEnemy, getLoot } = require("../core/loot");
+const bestiary = require("../core/bestiary");
+const { loadStats, saveStats } = require("../core/stats");
 const { addXP } = require("../core/xp");
+const { addGold } = require("../core/gold");
 const { addItem } = require("../core/inventory");
+const { playerDeath } = require("../core/death");
 
 module.exports = {
     name: "lov",
-    description: "Lov nepřátel a získávání lootů",
+    description: "Zaútočí na náhodného nepřítele a získá loot",
 
     execute: async (client, channel, user) => {
         const username = user.username.toLowerCase();
 
-        const enemy = getRandomEnemy();
-        const loot = getLoot(enemy);
+        const stats = loadStats();
+        const s = stats[username];
 
-        // XP za kill
-        const xpGain = enemy.level * 10;
+        if (!s) {
+            return client.say(channel, `@${username} ještě nemáš statistiky. Napiš !stats.`);
+        }
+
+        // Vybereme náhodného moba
+        const mob = bestiary[Math.floor(Math.random() * bestiary.length)];
+
+        // PvE damage výpočet
+        const dmgPlayer = Math.max(1, s.strength - (mob.defense || 0));
+        const dmgMob = Math.max(1, (mob.damage || mob.level * 2) - s.defense);
+
+        let hpPlayer = s.currentHP;
+        let hpMob = mob.hp || mob.level * 10;
+
+        // Simulace boje
+        while (hpPlayer > 0 && hpMob > 0) {
+            hpMob -= dmgPlayer;
+            if (hpMob <= 0) break;
+
+            hpPlayer -= dmgMob;
+        }
+
+        // Hráč prohrál
+        if (hpPlayer <= 0) {
+            const msg = playerDeath(username);
+            return client.say(channel, `@${username} ${msg}`);
+        }
+
+        // Hráč vyhrál
+        s.currentHP = hpPlayer;
+        saveStats(stats);
+
+        // XP + goldy
+        const xpGain = mob.level * 5;
+        const goldGain = mob.level * 3;
+
         addXP(username, xpGain);
+        addGold(username, goldGain);
 
-        // Uložení itemu do inventáře
-        addItem(username, {
-            name: loot.name,
-            rarity: loot.rarity,
-            enemy: enemy.name
-        });
+        // Loot
+        const drop = mob.drops[Math.floor(Math.random() * mob.drops.length)];
+        addItem(username, drop.name);
 
         return client.say(
             channel,
-            `@${username} ulovil **${enemy.name}** (lvl ${enemy.level}) a získal **${loot.rarity}** item: ${loot.name}! (+${xpGain} XP)`
+            `@${username} porazil jsi **${mob.name}**! Získáváš +${xpGain} XP, +${goldGain} goldů a item **${drop.name}**.`
         );
     }
 };
